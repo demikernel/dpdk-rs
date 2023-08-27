d897ed14b3703fa282ccd4f94430af2111084845
Owen Hilyard <oph1003@unh.edu>
2023-08-27 11:51:22 -0400

    Switch to Rust pkg-config library
    Rust has a pkg-config library that is maintained by the Rust foundation.
    Using this instead of the hand-written integration reduces the effort
    required to maintain the build system and also adds support for
    cross-compilation (something which is important to many people and
    organizations in the DPDK community). This library also handles a few
    edge-cases I ran into with the previous approach involving custom
    configurations of DPDK which link libbsd (a secondary -L argument), or
    statically link mlx5_core into librte_ethdev.

--- build.rs
+++ build.rs
@@ -167,73 +167,18 @@ fn os_build() -> Result<()> {
 
 #[cfg(target_os = "linux")]
 fn os_build() -> Result<()> {
-    let mut libdpdk_pkg_config = ::ya_pkg_config::Config::new();
+    let mut libdpdk_pkg_config = ::pkg_config::Config::new();
     libdpdk_pkg_config
-        // .statik(cfg!(static_dpdk))
-        .statik(true)
         .print_system_cflags(false)
         .print_system_libs(false);
 
-    let mut libdpdk = libdpdk_pkg_config
-        .probe("libdpdk-libs")
+    let libdpdk = libdpdk_pkg_config
+        .probe("libdpdk")
         // This unwrap will result in a compiler error for the user if libdpdk is not found
         .unwrap();
 
-    libdpdk.libs = libdpdk.libs.into_iter().filter(|lib| !lib.starts_with(':')).collect();
-
-
     let out_dir_s = env::var("OUT_DIR").unwrap();
     let out_dir = Path::new(&out_dir_s);
-
-    // println!("cargo:rerun-if-env-changed=PKG_CONFIG_PATH");
-    // let cflags_bytes = Command::new("pkg-config")
-    //     .args(&["--cflags", "libdpdk"])
-    //     .output()
-    //     .unwrap_or_else(|e| panic!("Failed pkg-config cflags: {:?}", e))
-    //     .stdout;
-    // let cflags = String::from_utf8(cflags_bytes).unwrap();
-
-    // let mut header_locations = vec![];
-
-    // for flag in cflags.split(' ') {
-    //     if flag.starts_with("-I") {
-    //         let header_location = flag[2..].trim();
-    //         header_locations.push(header_location);
-    //     }
-    // }
-
-    // let ldflags_bytes = Command::new("pkg-config")
-    //     .args(&["--libs", "libdpdk"])
-    //     .output()
-    //     .unwrap_or_else(|e| panic!("Failed pkg-config ldflags: {:?}", e))
-    //     .stdout;
-    // let ldflags = String::from_utf8(ldflags_bytes).unwrap();
-
-    // let mut library_location = None;
-    // let mut lib_names = vec![];
-
-    // for flag in ldflags.split(' ') {
-    //     if flag.starts_with("-L") {
-    //         library_location = Some(&flag[2..]);
-    //     } else if flag.starts_with("-l") {
-    //         lib_names.push(&flag[2..]);
-    //     }
-    // }
-
-    // // Link in `librte_net_mlx5` and its dependencies if desired.
-    // #[cfg(feature = "mlx5")]
-    // {
-    //     lib_names.extend(&["rte_net_mlx5", "rte_bus_pci", "rte_bus_vdev", "rte_common_mlx5"]);
-    // }
-
-    // // Step 1: Now that we've compiled and installed DPDK, point cargo to the libraries.
-    // if let Some(location) = library_location {
-    //     println!("cargo:rustc-link-search=native={}", location);
-    // }
-
-    // for lib_name in &lib_names {
-    //     println!("cargo:rustc-link-lib=dylib={}", lib_name);
-    // }
 
     let include_paths_utf8 = libdpdk.include_paths
         .iter()
@@ -250,7 +195,7 @@ fn os_build() -> Result<()> {
             },
         })
         .collect::<Vec<_>>();
-
+ 
     // Step 2: Generate bindings for the DPDK headers.
     let mut builder: Builder = Builder::default();
     for header_location in &include_paths_utf8 {
@@ -345,7 +290,6 @@ fn os_build() -> Result<()> {
         builder.include(header_location);
     }
     builder.compile("inlined");
-    // panic!("{:#?}", libdpdk);
     Ok(())
 }
 
